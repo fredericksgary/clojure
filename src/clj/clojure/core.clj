d947fa5ef2561a458ee1cdae4f814da557921b6e
Gary Fredericks <fredericksgary@gmail.com>
2013-05-26 14:57:59 -0500

    CLJ-1162: throw CCE wrt IDeref on (deref 1)
    When derefing inappropriate objects, it is more helpful to report that
    the object can't be cast to an IDeref than a
    java.util.concurrent.Future.

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -2124,13 +2124,25 @@
   value is available. See also - realized?."
   {:added "1.0"
    :static true}
-  ([ref] (if (instance? clojure.lang.IDeref ref)
-           (.deref ^clojure.lang.IDeref ref)
-           (deref-future ref)))
+  ([ref] (cond
+          (instance? clojure.lang.IDeref ref)
+          (.deref ^clojure.lang.IDeref ref)
+
+          (instance? java.util.concurrent.Future ref)
+          (deref-future ref)
+
+          :else ;; throw a ClassCastException
+          (cast clojure.lang.IDeref ref)))
   ([ref timeout-ms timeout-val]
-     (if (instance? clojure.lang.IBlockingDeref ref)
-       (.deref ^clojure.lang.IBlockingDeref ref timeout-ms timeout-val)
-       (deref-future ref timeout-ms timeout-val))))
+     (cond
+      (instance? clojure.lang.IBlockingDeref ref)
+      (.deref ^clojure.lang.IBlockingDeref ref timeout-ms timeout-val)
+
+      (instance? java.util.concurrent.Future ref)
+      (deref-future ref timeout-ms timeout-val)
+
+      :else ;; throw a ClassCastException
+      (cast clojure.lang.IDeref ref))))
 
 (defn atom
   "Creates and returns an Atom with an initial value of x and zero or
